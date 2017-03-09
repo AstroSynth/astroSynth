1f4549e2e0adc7075badd7580d6f8a6752e418ac
Thomas Boudreaux <thomas@boudreauxmail.com>
2017-03-09 10:00:59 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='astroSynth',
+      version='0.1',
+      description='Very Basic Astrophysics Synthetic Generation Suite',
+      url='https://github.com/tboudreaux/astroSynth.git',
+      author='Thomas Boudreaux',
+      author_email='thomas@boudreauxmail.com',
+      license='MIT',
+      packages=['astroSynth', 'tqdm', 'numpy', 'pandas', 'scipy'],
+      zip_safe=False)
